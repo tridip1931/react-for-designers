ecc701cdbc49cae5e4a036834cc0ec4c401da6df
Tridip Thrizu <tridip1931@gmail.com>
2019-10-17 19:21:27 +0800

    day 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Header />
     <div className="Hero">
       <div className="HeroGroup">
         <h1>
